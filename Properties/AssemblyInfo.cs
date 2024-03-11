c142248199ca8f8bd1b466979ea1a0599f1bbfd2
ker <ksdsksd@gmail.com>
2024-03-11 11:46:43 -0400

    Missing Properties folder

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GlobalStorageLockingAndLogging")]
+[assembly: AssemblyCompany("ZombsRCute")]
+[assembly: AssemblyProduct("GlobalStorageLockingAndLogging")]
+[assembly: AssemblyCopyright("Copyright ©2024")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
